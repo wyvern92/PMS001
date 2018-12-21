3d63b0036acab5db994cfaeb5cdf0baaee677677
wyvern92 <i271019477@qq.com>
2018-12-21 01:15:12 -0800

    user and admin function added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # skip_before_action ​:authorize
+  #
+  before_action :authorize
+
+  protected
+  def authorize
+    unless Authentication.find_by(id: session[:user_id])
+      redirect_to login_url, notice:"Please log in"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'admin' => 'admin#index'
+
   resources :authentications
   resources :houseinfos
+
+   controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+  get ':controller(/:action(/:id(.:format)))'
+  post ':controller(/:action(/:id(.:format)))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
